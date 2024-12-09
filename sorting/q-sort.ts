fc033daa800152e9d2dcf6b7e1bb1908847f1c51
Roman Melnyk <email.rom.melnyk@gmail.com>
2024-12-09 10:23:50 +0100

    Fix the q-sort

--- sorting/q-sort.ts
+++ sorting/q-sort.ts
@@ -1,29 +1,18 @@
 export function qSort<T>(arr: T[]): T[] {
   if (arr.length < 2) return arr
 
-  if (arr.length === 2) return arr[0] > arr[1]
-    // Swap if needed
-    ? [arr[1], arr[0]]
-    : arr
-
-  if (arr.length > 2) {
-    let center = Math.floor(arr.length / 2)
-    const centerEl = arr[center]
-
-    let low = 0
-    let high = arr.length - 1
-    while (low < center && high > center) {
-      while (arr[low] < centerEl && low < center) low++
-      while (arr[high] > centerEl && high > center) high++
-      if ()
-    }
-
-    const left = qSort(arr.slice(0, center))
-    const right = qSort(arr.slice(center + 1))
-    const result = centerEl > left[0]
-      ? [...le]
-    return left[0] > right[0]
-      ? [...right, ...left]
-      : [...left, ...right]
+  const center = Math.floor(arr.length / 2)
+  const centerEl = arr[center]
+  let left = [] as T[]
+  let right = [] as T[]
+  for (let i = 0; i < arr.length; i++) {
+    if (i === center) continue
+    const element = arr[i];
+    if (element <= centerEl) left.push(element)
+    else right.push(element)
   }
+
+  left = qSort(left)
+  right = qSort(right)
+  return [...left, centerEl, ...right]
 }
